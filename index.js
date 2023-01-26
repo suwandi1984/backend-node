4f3357d9bc23c4c02b9d8a80dc34c0ec66dfc4df
suwandi1984 <103377230+suwandi1984@users.noreply.github.com>
2023-01-27 05:51:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,15 +34,16 @@ app.use(cors());
 
 // Production //ssl off
 // create database connection Heroku Postgres
- const conn = new Pool({
-   user:'weufskczjfwcfj',
-   host:'ec2-34-192-210-139.compute-1.amazonaws.com',
-   database:'d14v82rrht9jdo',
-   password: '131ed1bb2508cfa80ed86e1392af78d928c0bb02f2546d9d5ab52e66f4ea2d2e',
-   port: 5432,
-   native: true,
-   ssl: true
- })
+// const conn = new Pool({
+//   user:'weufskczjfwcfj',
+//   host:'ec2-34-192-210-139.compute-1.amazonaws.com',
+//   database:'d14v82rrht9jdo',
+//   password: '131ed1bb2508cfa80ed86e1392af78d928c0bb02f2546d9d5ab52e66f4ea2d2e',
+//   port: 5432,
+//   native: true,
+//   ssl: true
+// })
+const conn = 'postgres://weufskczjfwcfj:131ed1bb2508cfa80ed86e1392af78d928c0bb02f2546d9d5ab52e66f4ea2d2e@ec2-34-192-210-139.compute-1.amazonaws.com:5432/d14v82rrht9jdo';
 
 
 //connect to database
