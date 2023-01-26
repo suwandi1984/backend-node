6281c37ec2c3097fb74fe3a2281fc10bc4a9eb15
suwandi1984 <103377230+suwandi1984@users.noreply.github.com>
2023-01-27 00:05:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -35,10 +35,10 @@ app.use(cors());
 // Production //ssl off
 // create database connection Heroku Postgres
  const conn = new Pool({
-   user:'postgres',
-   host:'localhost',
-   database:'to-kf-conim',
-   password: 'Junior00',
+   user:'weufskczjfwcfj',
+   host:'ec2-34-192-210-139.compute-1.amazonaws.com',
+   database:'d14v82rrht9jdo',
+   password: '131ed1bb2508cfa80ed86e1392af78d928c0bb02f2546d9d5ab52e66f4ea2d2e',
    port: 5432,
    native: true,
    ssl: {
